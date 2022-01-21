bcf8f01fb9c483a252455d72c62db85cf8a09d93
Anton <antonakulich1993@gmail.com>
2022-01-21 11:49:12 +0300

    initial commit

---
+++ LFP-Project-MVVMpattern/LFP-Project-MVVMpattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LFP-Project-MVVMpattern
+//
+//  Created by MacBook on 21.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
